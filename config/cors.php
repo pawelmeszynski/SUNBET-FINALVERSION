0df466f250fa0a6893e8ca2b602653c48a3098b9
Paweł <p.meszynski99@gmail.com>
2022-06-29 15:25:28 +0200

    disable authorization

--- config/cors.php
+++ config/cors.php
@@ -20,7 +20,7 @@ return [
      * You can enable CORS for 1 or multiple paths.
      * Example: ['api/*']
      */
-    'paths' => ['api/*'],
+    'paths' => ['localhost'],
 
     /*
     * Matches the request method. `[*]` allows all methods.
